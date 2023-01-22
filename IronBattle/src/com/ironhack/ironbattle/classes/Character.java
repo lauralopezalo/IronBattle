4fee6590db973214f0ec4fb9770246aa39a8e2d8
Emmascobar <emmanuelrescobar@gmail.com>
2023-01-22 23:50:10 +0100

    Warrior class

--- IronBattle/src/com/ironhack/ironbattle/classes/Character.java
+++ IronBattle/src/com/ironhack/ironbattle/classes/Character.java
@@ -1,2 +1,48 @@
-package com.ironhack.ironbattle.classes;public class Character {
+package com.ironhack.ironbattle.classes;
+
+public abstract class Character {
+    private String id;
+    private String name;
+    private int hp;
+    private boolean isAlive = true;
+
+    public Character(String name) {
+        this.id = "0125";
+        this.name = name;
+        this.hp = 100;
+        this.isAlive = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 }
+
--- IronBattle/src/com/ironhack/ironbattle/classes/Warrior.java
+++ IronBattle/src/com/ironhack/ironbattle/classes/Warrior.java
@@ -1,13 +1,69 @@
-package com.ironhack.ironbattle;
+package com.ironhack.ironbattle.classes;
+import com.ironhack.ironbattle.interfaces.Attacker;
 
-public class Warrior extends Character {
+public class Warrior extends Character implements Attacker {
     public int stamina;
     public int strength;
 
-    public Warrior(String name, int hp, int stamina, int strength) {
-        super(name, hp);
+    public Warrior(String name) {
+        super(name);
+        setStamina(stamina);
+        setStrength(strength);
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        stamina = (int)(Math.random()*50+10);
         this.stamina = stamina;
+
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        strength = (int)(Math.random()*10+1);
         this.strength = strength;
     }
 
+    @Override
+    public void attack(Character character) {
+
+        int randomAttack = 0;
+        int heavyAttack = strength;
+        int weakAttack = strength/2;
+
+
+            if ((int)(Math.random()*10+1)<5 && stamina >=1){
+                character.setHp(character.getHp() - randomAttack);
+                randomAttack = weakAttack;
+                stamina++;
+                System.out.println(character.getName() + " ha realizado el Ataque Debil," + "inflingiendo un daño de " + randomAttack + " sobre " + character.getName() + ". Tu stamina ahora es de: " + stamina);
+
+            }else if((int)(Math.random()*10+1)>5 && stamina >=5){
+                randomAttack = heavyAttack;
+                stamina-=5;
+                System.out.println(getName() + " ha realizado el Ataque Fuerte," + "inflingiendo un daño de " + randomAttack + " sobre " + character.getName() + ". Tu stamina ahora es de: " + stamina + ". El hp de " + character.getName() + " ahora es de " + character.getHp());
+            }else{
+                randomAttack = weakAttack;
+                stamina++;
+                System.out.println(getName() + " ha realizado el Ataque Debil," + "inflingiendo un daño de " + randomAttack + " sobre " + character.getName() + ". Tu stamina ahora es de: " + stamina + ". El hp de " + character.getName() + " ahora es de " + character.getHp());
+
+            }
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Character sheet: \nName: " + getName() +
+                " Ha realizado el ataque " +
+                "\nStamina: " + stamina + " points" +
+                "\nStrength: " + strength + " points" +
+                "\nHp: " + getHp() + " points\n";
+    }
 }
